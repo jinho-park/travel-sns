d3c688b2ad90f2c2cac8d207c76bb0538d1993a4
jinho-park <lucky112233335@gmail.com>
2017-12-27 23:30:55 +0900

    modified

--- travel-sns-webclient/src/components/App.js
+++ travel-sns-webclient/src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Home from 'components/home/Home';
 
 class App extends Component {
   render() {
     return (
       <div>
-        coding start
+        <Route exact path="/" component={Home}/>
       </div>
     );
   }
---
+++ travel-sns-webclient/src/components/home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () =>{
+    return(
+        <div>
+            코딩 시작
+        </div>
+    )
+}
+
+export default Home;
