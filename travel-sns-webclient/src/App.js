e22c0e9fbf20c261e2e1086e8690a91663704f28
jinkyu2005 <jinkyu2005@naver.com>
2017-12-28 19:20:43 +0900

    [2017-12-28]

--- travel-sns-webclient/src/App.js
+++ travel-sns-webclient/src/App.js
@@ -2,20 +2,51 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// CommentBox 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+  render() { 
+    return ( 
+      <div className="storyBox"> 
+        <center>
+          <h1>여행 기록</h1>
+        </center>
+        <StoryList {...this.props} /> 
+      </div>  
+    );  
+  }  
+}
+
+class StoryList extends Component { 
+  render() { 
+    var storys = this.props.data.map(function(story, index){ 
+      return <Story key={index}>{story}</Story>  
+    }.bind(this));  
+    return ( 
+      <div className="storyList">  {storys} </div>
+    );  
+  }  
+}
+
+class Story extends Component{
+  render() { 
+    return ( 
+      <div className="story"> 
+        {this.props.children.isFirst == true ? '여행을 시작해 보세요' : this.renderStorys()}
+      </div>  
+    );
+  }
+
+  renderStorys(){
+    return(
+      <div>
+        <h2>  
+          {this.props.children.author} 
+        </h2>
+        {this.props.children.text} 
       </div>
     );
   }
 }
+
 
 export default App;
--- travel-sns-webclient/src/index.js
+++ travel-sns-webclient/src/index.js
@@ -4,5 +4,15 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+{
+    var data = [  
+        {author : "null", text : "null", isFirst : true},  
+        {author : "작성자1", text : "글1", isFirst : false},  
+        {author : "작성자2", text : "글2", isFirst : false},  
+        {author : "작성자3", text : "글3", isFirst : false},  
+        {author : "작성자4", text : "글4", isFirst : false} 
+    ];
+}
+
+ReactDOM.render(<App data={data}/>, document.getElementById('root'));
 registerServiceWorker();
